d68a469934a53ab063bb280e1e648d4b03948af3
Peterson "Prize" Adami Candido <contact.petersonac@protonmail.com>
2022-11-21 17:34:59 -0300

    favicon fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,21 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 import styles from "../styles/Monitor.module.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={styles.monitor}>
-      <section className={styles.monitor} />
-      <section className={styles.scan} />
-      <section className={styles.main}>
-        <Component {...pageProps} />
-      </section>
-    </main>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.monitor}>
+        <section className={styles.monitor} />
+        <section className={styles.scan} />
+        <section className={styles.main}>
+          <Component {...pageProps} />
+        </section>
+      </main>
+    </>
   );
 }
